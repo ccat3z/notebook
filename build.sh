9c0bf34a477e58d14f0ab0066c2de12d988065e6
ccat3z <fzhang.chn@foxmail.com>
2021-09-06 09:45:15 +0800

    shellcheck

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-export NOTE_ROOT=$(realpath $(dirname $0))
+NOTE_ROOT=$(realpath "$(dirname "$0")")
 
 export OLD_PATH="$PATH"
 export PATH="$NOTE_ROOT/bin:$PATH"
 
 if [ -n "$1" ]; then
     FILES="$1"
-    OUT="$(echo $1 | sed 's/\.[^\.]*$//')"
+    OUT="$(echo "$1" | sed 's/\.[^\.]*$//')"
 else
-    FILES="$(ls *.md)"
+    FILES="$(ls ./*.md)"
     OUT="out"
 fi
 
 pandoc \
     --pdf-engine=xelatex \
     --pdf-engine-opt=-shell-escape \
-    --template $NOTE_ROOT/template.tex \
+    --template "$NOTE_ROOT/template.tex" \
     --top-level-division=chapter \
     --filter center.py \
     --filter example.py \
@@ -24,5 +24,5 @@ pandoc \
     --filter makeindex.py \
     --filter python.py \
     --filter plantuml.py \
-    --data-dir $NOTE_ROOT \
-    ${FILES} -o ${OUT}.pdf
+    --data-dir "$NOTE_ROOT" \
+    "${FILES}" -o "${OUT}.pdf"
